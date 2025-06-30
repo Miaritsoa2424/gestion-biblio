5c035215543941cd46e0bef3d609e686edfda434
miaritsoa2424 <miaritsoa24@gmail.com>
2025-07-01 00:13:19 +0300

    Mise a jour du deploiement

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 mvn clean package
-cp target/biblio-spring-1.0.war /home/zark/Documents/apache-tomcat-10.1.28/webapps
-# /home/zark/Documents/apache-tomcat-10.1.28/bin/startup.sh
+cp target/biblio-spring-1.0.war /home/miaritsoa/ITU/webDynamique/apache-tomcat-10.1.28/webapps
+/home/miaritsoa/ITU/webDynamique/apache-tomcat-10.1.28/bin/shutdown.sh
+/home/miaritsoa/ITU/webDynamique/apache-tomcat-10.1.28/bin/startup.sh
+
+# /home/miaritsoa/ITU/webDynamique/apache-tomcat-10.1.28/webapps
